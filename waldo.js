2420e6c378481af735e31a42000692ce1659975a
michepriest <miche.priest@gmail.com>
2018-09-27 19:15:54 +0000

    refactored Waldo with forEach

--- waldo.js
+++ waldo.js
@@ -1,18 +1,16 @@
 var result = [];
 
 function findWaldo(arr, found) {
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] === "Waldo") {
+  arr.forEach(function(word, i) {
+    if (word === "Waldo") {
       result = i; 
-      found();  
-    }
-  }
+      found(i);  
+      }
+  })
+};
 
-  console.log(result)
-}
-
-function actionWhenFound() {
-  console.log("Found Waldo at index " + result + "!");
+function actionWhenFound(index) {
+  console.log("Found Waldo at index " + index + "!");
 }
 
 findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
